9609d8a8dca465a97f35c13a4efe015092dabe05
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2024-01-18 14:41:34 +0300

    Created inventory service,controller and repository

--- inventory-service/src/main/java/com/isg/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/isg/inventoryservice/repository/InventoryRepository.java
@@ -3,6 +3,9 @@ package com.isg.inventoryservice.repository;
 import com.isg.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
+    Optional<Inventory> findBySkuCode(String skuCode);
 }
