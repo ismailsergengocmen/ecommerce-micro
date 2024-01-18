b851132ef3d0f30c65c610d22d7ce2d80a8edcb1
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2024-01-18 17:35:03 +0300

    Update endpoint to check multiple skucode instead of one

--- inventory-service/src/main/java/com/isg/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/isg/inventoryservice/service/InventoryService.java
@@ -1,9 +1,12 @@
 package com.isg.inventoryservice.service;
 
+import com.isg.inventoryservice.dto.InventoryResponse;
 import com.isg.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,8 +15,12 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode){
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0).build()
+                ).toList();
     }
-
 }
