86bb860531bb62ee35e2e7f7c439b802728163ee
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2024-01-18 17:34:13 +0300

    Added support for inter service communication to order service

--- order-service/src/main/java/com/isg/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/isg/orderservice/service/OrderService.java
@@ -5,8 +5,10 @@ import com.isg.orderservice.dto.OrderRequest;
 import com.isg.orderservice.model.Order;
 import com.isg.orderservice.model.OrderLineItems;
 import com.isg.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -14,14 +16,11 @@ import java.util.UUID;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
-
-    @Autowired
-    public OrderService(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
-    }
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -31,6 +30,17 @@ public class OrderService {
                 .stream().map(this::mapToDto).toList();
 
         order.setOrderLineItemsList(orderLineItems);
+
+        // Call Inventory Service and place order if product is in stock
+        // webClient make a synchronous request to given endpoint
+        Boolean result = webClient.get().uri("http://localhost:8082/api/inventory")
+                .retrieve().bodyToMono(Boolean.class).block();
+
+        if(result){
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
         orderRepository.save(order);
     }
 
