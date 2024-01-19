dbfb6f1dee9dba5eef89074f023f73c2462e67c5
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2024-01-19 21:34:30 +0300

    Added retry and timeout traits to circuit breaker

--- order-service/src/main/java/com/isg/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/isg/orderservice/controller/OrderController.java
@@ -3,8 +3,12 @@ package com.isg.orderservice.controller;
 import com.isg.orderservice.dto.OrderRequest;
 import com.isg.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -20,15 +24,19 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     //? Every call under this method will be in circuit breaker's scope
     //? fallbackMethod takes a function to be used as callback function
+    //? Name should be same with the one used in application.properties
+    //? The method should now return CompletableFuture<String>, since it will become
+    //? async call when the time limiter used
     @CircuitBreaker(name= "inventory", fallbackMethod = "fallbackMethod")
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Sucessfully";
+    @TimeLimiter(name="inventory")
+    @Retry(name="inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
     }
 
     //? The fallback method of circuit breaker should have same return type
     //? with original method covered by circuit breaker
-    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
-        return "Oops! Something went wrong, please order after some time!";
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
     }
 }
