05f9c7dc9c19f8f69a202a51fd20d3b96e652b88
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2024-01-19 15:14:54 +0300

    Added api gateway

--- order-service/src/main/java/com/isg/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/isg/orderservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.isg.orderservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,8 +8,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    // Client-size load balancing capabilities
+    // to the web client builder
     @Bean
-    public WebClient webClient(){
-        return WebClient.builder().build();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder(){
+        return WebClient.builder();
     }
 }
