cbe9673cab7d3dee50d6184721af4b315737787f
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2024-01-19 21:06:41 +0300

    Added circuit breaker for place order function

--- order-service/src/main/java/com/isg/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/isg/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.isg.orderservice.controller;
 
 import com.isg.orderservice.dto.OrderRequest;
 import com.isg.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,8 +18,17 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    //? Every call under this method will be in circuit breaker's scope
+    //? fallbackMethod takes a function to be used as callback function
+    @CircuitBreaker(name= "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Placed Sucessfully";
+    }
+
+    //? The fallback method of circuit breaker should have same return type
+    //? with original method covered by circuit breaker
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Oops! Something went wrong, please order after some time!";
     }
 }
