c90d933c89e06c91f87c4a900d2d032f99b00227
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2024-01-21 15:28:34 +0300

    Added kafka, prometheus, grafana and dockerized the application

---
+++ notification-service/src/main/java/com/isg/OrderPlacedEvent.java
@@ -0,0 +1,13 @@
+package com.isg;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+    private String orderNumber;
+}
--- order-service/src/main/java/com/isg/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/isg/orderservice/service/OrderService.java
@@ -3,13 +3,13 @@ package com.isg.orderservice.service;
 import com.isg.orderservice.dto.InventoryResponse;
 import com.isg.orderservice.dto.OrderLineItemsDto;
 import com.isg.orderservice.dto.OrderRequest;
+import com.isg.orderservice.event.OrderPlacedEvent;
 import com.isg.orderservice.model.Order;
 import com.isg.orderservice.model.OrderLineItems;
 import com.isg.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.sleuth.Span;
-import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -27,47 +27,46 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
-    private final Tracer tracer;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
-    public String placeOrder(OrderRequest orderRequest){
+    public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
-                .stream().map(this::mapToDto).toList();
+                .stream()
+                .map(this::mapToDto)
+                .toList();
 
         order.setOrderLineItemsList(orderLineItems);
 
         List<String> skuCodes = order.getOrderLineItemsList().stream()
-                .map(OrderLineItems::getSkuCode).toList();
+                .map(OrderLineItems::getSkuCode)
+                .toList();
 
-        log.info("Calling inventory service");
-        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
 
-        try (Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())){
-            // Call Inventory Service and place order if product is in stock
-            // webClient make a synchronous request to given endpoint
-            InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
-                    .uri("http://inventory-service/api/inventory",
-                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-                    .retrieve().bodyToMono(InventoryResponse[].class).block();
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                .allMatch(InventoryResponse::isInStock);
 
-            boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
-
-            if(allProductsInStock){
-                orderRepository.save(order);
-                return "Order Placed Successfully";
-            } else {
-                throw new IllegalArgumentException("Product is not in stock, please try again later");
-            }
-        } finally {
-            inventoryServiceLookup.end();
+        if (allProductsInStock) {
+            orderRepository.save(order);
+            // publish Order Placed Event
+            applicationEventPublisher.publishEvent(new OrderPlacedEvent(order.getOrderNumber()));
+            return "Order Placed";
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
         }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
         OrderLineItems orderLineItems = new OrderLineItems();
-        orderLineItems.setPrice(orderLineItems.getPrice());
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
         orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
         orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
         return orderLineItems;
